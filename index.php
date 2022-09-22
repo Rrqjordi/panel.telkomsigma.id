9f2d5b539dd97dca9d28dd8f662a23bfe8e7045a
Rrqjordi <113660400+Rrqjordi@users.noreply.github.com>
2022-09-22 22:13:20 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Whooopzzzzz</title> 
+<meta property="og:image"content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrIjzk6MDVBTKr9kFqvMaKmg9lrSlQ9UTKvQ"> 
+<meta name="description" content="I'am Just A Devil"> <meta name="keywords" content="Ghost Riddiculous Team"> 
+<meta http-equiv="cache-control" content="index,cache"> 
+<meta http-equiv="pragma" content="index,cache"> 
+  <style>
+html{
+background-color:#FFFFFF;
+color: #001933;
+font-family: Playball
+}
+    a:hover {
+      color: #001933;
+    }
+</style>
+<br><center>
+<img src="https://c.top4top.io/p_2456w0k6n0.png"><br>
+</style> 
+
+ </head> 
+ <body>
+  <table height="30%" width="100%"> 
+   <body>
+    <tr>
+     <p>
+	<br><br><br><br><br><td align="center">
+<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet" type="text/css">
+ <div id="q">
+   <br><br><br><br><br><br><br><br><br><center><span class="blinking"><font face ="Shadows Into Light Two" font size ="50">HACKED BY ./RRQ_JORDI</font></span></center>
+<style type="text/css">BODY{cursor:none}</style> </font>
+   </div>
+  </center>
+  <font face="Shadows Into Light Two"> 
+   <center> 
+    <style type="text/css">BODY{cursor:none}</style> 
+    <style type="text/css">
+.blinking{
+    animation:blinkingText 0.8s infinite;
+}
+@keyframes blinkingText{
+    0%{     color: #ff0027;    }
+    20%{    color: #0c00ff; }
+    40%{    color: white; }
+	60%{    color: #00e800; }
+	80%{    color: red; }
+    100%{   color: #d0ff00;    }
+}
+</style>
+<style>
+.blink {
+  animation: blink-animation 1s steps(5, start) infinite;
+  -webkit-animation: blink-animation 1s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+
+
+</style>
+<span class="blink"> -=[My Whole Life Is Just A Dream]=- </span>
+<br><br>
+
+
+
+</body>
+
+</br></b></font></td></tr></tbody></table></center></a></td></tr></tbody></table></center></body></html>
